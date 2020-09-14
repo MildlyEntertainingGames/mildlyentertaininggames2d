a5c54ada98688b88fa672268a10b6ee1afa89b29
Jan-Erik <jan-erik.baehr@itsnothing.de>
2020-09-15 01:46:05 +0200

    added main menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        //TODO load game scene
+    }
+
+    public void OpenWebsite()
+    {
+        Application.OpenURL("https://mildlyentertaining.games");
+    }
+}
